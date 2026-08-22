418a93e6299d35c59cbfe6951b02c155f7fd7b52
Cocotrilo <nogalesisaac25@gmail.com>
2026-08-21 21:30:33 -0500

    sleep canceling and mob horde spawns near you with the brand in the night

--- src/main/java/com/cocot/berserkmod/util/ModEventHandler.java
+++ src/main/java/com/cocot/berserkmod/util/ModEventHandler.java
@@ -5,6 +5,7 @@ import net.minecraft.client.audio.Sound;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.item.ItemEntity;
 import net.minecraft.entity.monster.SkeletonEntity;
+import net.minecraft.entity.monster.SpiderEntity;
 import net.minecraft.entity.monster.WitherSkeletonEntity;
 import net.minecraft.entity.monster.ZombieEntity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -16,8 +17,10 @@ import net.minecraft.util.SoundEvent;
 import net.minecraft.util.SoundEvents;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TextFormatting;
+import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
+import net.minecraftforge.event.entity.player.PlayerSleepInBedEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 import java.util.Random;
@@ -28,7 +31,7 @@ public class ModEventHandler {
     public void onLivingDrops(LivingDropsEvent event) {
         if (event.getEntity() instanceof WitherSkeletonEntity) {
             System.out.println("EVENTO MUERTE");
-            if (RANDOM.nextFloat() < 0.9F) {
+            if (RANDOM.nextFloat() < 0.15F) {
                 ItemStack fang = new ItemStack(RegistryHandler.BEAST_FANG.get());
                 event.getDrops().add(new ItemEntity(event.getEntity().world,
                         event.getEntity().getPosX(),
@@ -52,7 +55,7 @@ public class ModEventHandler {
                 player.addPotionEffect(new EffectInstance(RegistryHandler.SACRIFICE_BRAND.get(), 999999));
                 ItemStack behelitStack = player.inventory.decrStackSize(player.inventory.getSlotFor(new ItemStack(RegistryHandler.CRIMSON_BEHELIT.get())), 1);
 
-                for (int i = 0; i < 12; i++) {
+                for (int i = 0; i < 25; i++) {
                     if (i % 2 == 0) {
                         ZombieEntity zombie = new ZombieEntity(EntityType.ZOMBIE, player.world);
                         zombie.setPosition(player.getPosX() + i, player.getPosY(), player.getPosZ());
@@ -62,11 +65,38 @@ public class ModEventHandler {
                         skeleton.setPosition(player.getPosX() + i, player.getPosY(), player.getPosZ());
                         player.world.addEntity(skeleton);
                     }
-
                 }
             }
-
         }
-
+    }
+    @SubscribeEvent
+    public void onPlayerTick(TickEvent.PlayerTickEvent event) {
+        PlayerEntity player = event.player;
+        if (player.isPotionActive(RegistryHandler.SACRIFICE_BRAND.get())) {
+            if (RANDOM.nextFloat() < 0.001F && !player.world.isDaytime()) {
+                for (int i = 0; i < 7; i++) {
+                    if (i % 3 == 0) {
+                        ZombieEntity zombie = new ZombieEntity(EntityType.ZOMBIE, player.world);
+                        zombie.setPosition(player.getPosX() + i, player.getPosY(), player.getPosZ());
+                        player.world.addEntity(zombie);
+                    } else if (i % 3 == 1) {
+                        SkeletonEntity skeleton = new SkeletonEntity(EntityType.SKELETON, player.world);
+                        skeleton.setPosition(player.getPosX() + i, player.getPosY(), player.getPosZ());
+                        player.world.addEntity(skeleton);
+                    } else {
+                        SpiderEntity spider = new SpiderEntity(EntityType.SPIDER, player.world);
+                        spider.setPosition(player.getPosX() + i, player.getPosY(), player.getPosZ());
+                        player.world.addEntity(spider);
+                    }
+                }
+            }
+        }
+    }
+    @SubscribeEvent
+    public void onPlayerSleep (PlayerSleepInBedEvent event) {
+        PlayerEntity player = event.getPlayer();
+        if (player.isPotionActive(RegistryHandler.SACRIFICE_BRAND.get())) {
+            event.setCanceled(true);
+        }
     }
 }
